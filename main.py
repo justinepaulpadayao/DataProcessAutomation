067906e8bf26f607d261071786aa6e815d71e043
jpadayao <justpaulpadayao@gmail.com>
2023-01-04 11:59:21 +0800

    Fixed the code to save the csv output in the folder where the CSVs are being processed.

--- main.py
+++ main.py
@@ -7,14 +7,10 @@ from data_functions import read_csv_files, save_combined_csv, save_new_columns_c
 
 
 def main():
-    logging_config.configure_logging()  # Configure the logging
-    root_path = r"I:\Shared drives\Data Analysis\Data\Bank Data"
+    logger = logging_config.configure_logging()  # Configure the logging
+    root_path = r"I:\Shared drives\Data Analysis\Data\Bank Downloads 2022"
     try:
-        df_list, new_columns_list, root = read_csv_files(root_path)
-        save_combined_csv(df_list, root,
-                          f'Combined Transactions Data {datetime.datetime.now().strftime("%Y-%m-%d %H-%M-%S")}.csv')
-        save_new_columns_csv(new_columns_list, root,
-                             f'New Columns {datetime.datetime.now().strftime("%Y-%m-%d %H-%M-%S")}.csv')
+        read_csv_files(root_path)
         logging.info('Code ran successfully and without errors')  # Log a message if the code ran successfully
     except Exception as e:
         logging.exception(e)  # Log the exception if an error occurred
